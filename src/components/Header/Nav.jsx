5c008fb74c51797ab1ed8b8cdc38fd17bfacb13b
Albina Sabitova <albina.sabitovaa@icloud.com>
2024-08-11 04:18:37 +0200

    add header

---
+++ src/components/Header/Nav.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+function Nav({ isToggle }) {
+  return (
+    <ul
+      className={`navbar__list-link ${
+        !isToggle ? "navbar__list-link-active" : ""
+      }`}
+    >
+      <li href="" className="link">
+        <NavLink className="navlink" to={"/"}>
+          {" "}
+          Main Page
+        </NavLink>
+      </li>
+      <li href="" className="link">
+        <NavLink className="navlink" to={"categories"}>
+          {" "}
+          Categories
+        </NavLink>
+      </li>
+      <li href="" className="link">
+        <NavLink className="navlink" to={"products"}>
+          {" "}
+          All producst
+        </NavLink>
+      </li>
+      <li href="" className="link">
+        <NavLink className="navlink" to={"sales"}>
+          {" "}
+          All sales
+        </NavLink>
+      </li>
+      <div className="discount_menu">1 day discount!</div>
+    </ul>
+  );
+}
+
+export default Nav;
