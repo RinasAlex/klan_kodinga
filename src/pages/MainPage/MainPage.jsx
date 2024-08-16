bcbc6f7c1012fdb39c1ae1cfdaa9a463ee4c3e9f
Albina Sabitova <albina.sabitovaa@icloud.com>
2024-08-16 18:39:12 +0600

    add discount

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import "./MainPage.scss";
 import Categories from "@/components/Categories/Categories";
-import Banner from '@/components/Header/Banner'
+import Banner from "@/components/Header/Banner";
+import HomeDiscount from "../../components/HomeDiscount/HomeDiscount";
 
 const MainPage = () => {
   return (
     <>
-    <Banner/>
-    <Categories />
+      <Banner />
+      <Categories />
+      <HomeDiscount />
     </>
-  )
-}
+  );
+};
 
 export default MainPage;
