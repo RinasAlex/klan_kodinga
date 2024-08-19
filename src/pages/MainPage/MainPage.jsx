14f37088fa151431a0ffb38f5e334554f95b2b3c
Kseniia Mishchenko <ksusha.mischenko@gmail.com>
2024-08-20 00:39:42 +0200

    added randomSale

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./MainPage.scss";
 import Categories from "@/components/Categories/Categories";
 import Banner from "@/components/Header/Banner";
+import RandomSale from "../../components/RandomSale/RandomSale";
 
 const MainPage = () => {
   return (
     <>
       <Banner />
       <Categories />
+      <RandomSale />
     </>
   );
 };
