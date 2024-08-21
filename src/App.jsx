c8af3026c041f5aed2547666421be57d881bba0c
RinasAlex <alexandrarinas777@gmail.com>
2024-08-21 10:01:25 +0200

    updated AllSales page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import MainPage from "./pages/MainPage/MainPage";
@@ -6,9 +6,20 @@ import AllProductsPage from "./pages/AllProductsPage/AllProductsPage";
 import AllSalesPage from "./pages/AllSalesPage/AllSalesPage";
 import CartPage from "./pages/CartPage/CartPage";
 import CategoriesPage from "./pages/CategoriesPage/CategoriesPage";
+import { useDispatch, useSelector } from "react-redux";
+import { getProducts } from "./store/futures/productSlice";
 
 const App = () => {
+// const dispatch = useDispatch();
+// const {loading} = useSelector(state => state.products) 
 
+// useEffect(() => {
+//   dispatch(getProducts())
+// }, [dispatch])
+
+// if(loading){
+//   return "Loading ...."
+// }
 
   return (
     <>
