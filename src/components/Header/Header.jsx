6cf0d628750a36a0e45e6c36ad076b5178af71ff
Albina Sabitova <albina.sabitovaa@icloud.com>
2024-08-25 22:04:57 +0600

    add product page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,38 +7,66 @@ import sun from "../../assets/headerImages/sun.svg";
 import moon from "../../assets/headerImages/moon.svg";
 import Burger from "./Burger";
 import { NavLink } from "react-router-dom";
-import Nav from "./Nav";
 
 function Header() {
   const [isToggle, setIsToggle] = useState(false);
   return (
-    <nav className="navbar">
-      <div className="navbar__logo">
-        <NavLink to="/">
-          {" "}
-          <img className="logo" src={logo} alt="" />
-        </NavLink>
-        <label className="switch">
-          <input className="switch__input" type="checkbox" />
-          <span className="switch__slider"></span>
-          <img className="sun" src={sun} alt="" />
-          <img className="moon" src={moon} alt="" />
-        </label>
-      </div>
+    <nav className="navbar__container">
+      <div className="navbar">
+        <div className="navbar__logo">
+          <NavLink to="/">
+            <img className="logo" src={logo} alt="" />
+          </NavLink>
+          <label className="switch">
+            <input className="switch__input" type="checkbox" />
+            <span className="switch__slider"></span>
+            <img className="sun" src={sun} alt="" />
+            <img className="moon" src={moon} alt="" />
+          </label>
+        </div>
 
-      <div className="navbar__list">
-        <div className="discount">1 day discount!</div>
-        <Burger isToggle={isToggle} setIsToggle={setIsToggle} />
-        <Nav isToggle={isToggle} />
-      </div>
+        <div className="navbar__list">
+          <div className="discount">1 day discount!</div>
+          <Burger isToggle={isToggle} setIsToggle={setIsToggle} />
+          <div
+            className={`navbar__list-link ${
+              !isToggle ? "navbar__list-link-active" : ""
+            }`}
+          >
+            <ul className="link__container">
+              <li className="link">
+                <NavLink className="navlink" to={"/"}>
+                  Main Page
+                </NavLink>
+              </li>
+              <li className="link">
+                <NavLink className="navlink" to={"categories"}>
+                  Categories
+                </NavLink>
+              </li>
+              <li className="link">
+                <NavLink className="navlink" to={"products"}>
+                  All producst
+                </NavLink>
+              </li>
+              <li className="link">
+                <NavLink className="navlink" to={"sales"}>
+                  All sales
+                </NavLink>
+              </li>
+            </ul>
+            <div className="discount_menu">1 day discount!</div>
+          </div>
+        </div>
 
-      <div className="navbar__cart">
-        <NavLink>
-          <img className="heard" src={heart} alt="" />
-        </NavLink>
-        <NavLink>
-          <img className="cart" src={cart} alt="" />
-        </NavLink>
+        <div className="navbar__cart">
+          <NavLink>
+            <img className="heard" src={heart} alt="" />
+          </NavLink>
+          <NavLink>
+            <img className="cart" src={cart} alt="" />
+          </NavLink>
+        </div>
       </div>
     </nav>
   );
---
+++ src/pages/ProductsPage/ProductPage.jsx
@@ -0,0 +1,113 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchProductsById } from "../../store/futures/productSlice";
+import { NavLink, useLocation } from "react-router-dom";
+import "./ProductPage.scss";
+import heart from "../../assets/image/heart.png";
+
+function ProductPage() {
+  const [readMore, setReadMore] = useState(false);
+  const styleDiscription = {
+    display: readMore ? "block" : "-webkit-box",
+  };
+
+  const openText = () => {
+    setReadMore(!readMore);
+  };
+
+  const location = useLocation();
+  const { state } = location;
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchProductsById(state.id));
+  }, []);
+
+  const { product, loading } = useSelector((state) => state.products);
+  console.log(product, state);
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className="product__container">
+      <div className="container__box">
+        <div className="links">
+          <NavLink to={"/"} className="link__main">
+            Main Page
+          </NavLink>
+          <div className="feature"></div>
+          <NavLink to={"/categories"} className="link__main">
+            Category
+          </NavLink>
+          <div className="feature"></div>
+          {/* <NavLink to={"/"} className="link__main">
+          </NavLink> */}
+          <div className="feature"></div>
+          <p className="link">{state.title}</p>
+        </div>
+        {product &&
+          product.map((item) => (
+            <div key={item.id} className="item__box">
+              <div className="title__heart-480">
+                <h3 className="title">{item.title}</h3>
+                <img className="heart" src={heart} alt="" />
+              </div>
+              <div className="product__box">
+                <img
+                  className="product__img"
+                  src={`https://exam-server-5c4e.onrender.com/${item.image}`}
+                  alt=""
+                />
+                <div className="info__container">
+                  <div className="title__heart">
+                    <h3 className="title">{item.title}</h3>
+                    <img className="heart" src={heart} alt="" />
+                  </div>
+                  <div className="price__container">
+                    <p className="price">${item.price}</p>
+                    {item.discont_price ? (
+                      <p className="discount__price">${item.discont_price}</p>
+                    ) : null}
+                    {item.discont_price ? (
+                      <div className="sale">{`- ${Math.ceil(
+                        100 - item.discont_price / (item.price / 100)
+                      )} %`}</div>
+                    ) : null}
+                  </div>
+                  <p>{item.price_discont}</p>
+                  <div className="counter__container">
+                    <div className="counter__box">
+                      <button className="counter__btn">-</button>0
+                      <button className="counter__btn">+</button>
+                    </div>
+                    <button className="add__to">Add to cart</button>
+                  </div>
+                  <div className="description__container">
+                    <h5>Discription</h5>
+                    <p style={styleDiscription} className="description">
+                      {item.description}
+                    </p>
+                    <p className="read__more" onClick={openText}>
+                      Read more
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="description__container-768">
+                <h5>Discription</h5>
+                <p style={styleDiscription} className="description">
+                  {item.description}
+                </p>
+                <p className="read__more" onClick={openText}>
+                  Read more
+                </p>
+              </div>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+}
+
+export default ProductPage;
