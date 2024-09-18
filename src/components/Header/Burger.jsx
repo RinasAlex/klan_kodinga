dcbb3380f6b2657353a6762ffef8bb3b8cceb858
RinasAlex <151784662+RinasAlex@users.noreply.github.com>
2024-09-18 20:28:39 +0200

    fixed

--- src/components/Header/Burger.jsx
+++ src/components/Header/Burger.jsx
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./Burger.scss";
+import { ThemeContext } from "../ThemeProvider/ThemeProvider";
 
 function Burger({ isToggle, setIsToggle }) {
+  const { theme } = useContext(ThemeContext);
+
+
   return (
-    <div className="container__burger">
+    
+    <div className={`container__burger ${theme === 'dark' ? 'dark-border' : ''}`}>
       <div
-        onClick={() => setIsToggle(!isToggle)}
-        className={isToggle ? "burger active" : "burger"}
-      >
-        <span className="span"></span>
+       onClick={() => setIsToggle(!isToggle)}
+       className={isToggle ? "burger active" : "burger"}
+     >
+       <span className="span"></span>
       </div>
     </div>
+
   );
 }
 
